183f55f95930aeb82238081446eee448179f76d2
Betul <betulkoru06@gmail.com>
2024-03-09 11:36:45 +0300

    strcture finished

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 require('dotenv').config()
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 
-app.listen(PORT, () => console.log(`Server is running on -> ${HOST}:${PORT}`))
+app.all('/', (req, res) => {
+    res.send('wellcome to my blog api')
+})
+
+app.use(require('./src/errorHandler'))
+
+app.listen(PORT, () => console.log(`Server is running on -> http://${HOST}:${PORT}`))
