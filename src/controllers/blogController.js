c7062d5a403249365874d6c4bb8282255914500a
Betul <betulkoru06@gmail.com>
2024-03-09 13:47:31 +0300

    create route

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -16,5 +16,34 @@ module.exports.blogPost = {
             data: data,
         })
     },
+    create: async(req, res) => {
+        const data = await blogPost.create(req.body)
+        res.status(201).send({
+            error: false,
+            body: req.body,
+            data: data,
+        })
+    },
+    read: async(req, res) => {
+        const data = await blogPost.find()
+        res.status(200).send({
+            error: false,
+            data: data,
+        })
+    },
+    update: async(req, res) => {
+        const data = await blogPost.find()
+        res.status(200).send({
+            error: false,
+            data: data,
+        })
+    },
+    delete: async(req, res) => {
+        const data = await blogPost.find()
+        res.status(200).send({
+            error: false,
+            data: data,
+        })
+    },
 
 }
--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -5,6 +5,15 @@ const router = require('express').Router()
 const {route} = require('express/lib/router')
 const {blogPost} = require('../controllers/blogController')
 
-router.route('/posts').get(blogPost.list)
+router.route('/posts')
+    .get(blogPost.list)
+    .post(blogPost.create)
+
+router.route('/posts/:postId')
+    .get(blogPost.read)
+    .put(blogPost.update)  
+    .patch(blogPost.update) //put and patch is the same
+    .delete(blogPost.delete)
+
 
 module.exports = router;
