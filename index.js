c1d236e056b08c9bddec462b33cb8010a591ab69
Betul <betulkoru06@gmail.com>
2024-03-09 11:27:12 +0300

    index.js intro

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 
 const express = require('express')
 const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+app.listen(PORT, () => console.log(`Server is running on -> ${HOST}:${PORT}`))
