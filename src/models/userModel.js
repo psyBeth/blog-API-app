a6791d3daa0e8b2557f7aa7c2e69298112b8a26e
Betul <betulkoru06@gmail.com>
2024-03-11 23:04:59 +0300

    validating e-mail type

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,8 +8,22 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
-        required: true,
-        unique: true,
+        required: [true, 'E-mail is required.'],
+        // validate: (email) => {return true},
+        // validate: [
+        //     (email) => {
+        //         if (email.includes('@') && email.includes('.')) {
+        //             return true
+        //         }
+        //         return false
+        //     },
+        //     'Invalid e-mail type.'
+        // ],
+        validate: [
+            (email) => (email.includes('@') && email.includes('.')),
+            'Invalid e-mail type.'
+        ],
+        // unique: true,
     },
     password: {
         type: String,
