e13ff709ab0903414696356c23606d5a9e9ffd21
Betul <betulkoru06@gmail.com>
2024-03-11 23:39:38 +0300

    password encryption function built in module cyrpt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,6 +2,19 @@
 
 const mongoose = require('mongoose')
 
+// PASWORD ENCRYPTION
+// https://nodejs.org/api/cryptohtml#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+const crypto = require('node:crypto')
+
+const keyCode = process.env?.SECRET_KEY || 'write_random_chars_in_here'
+const loopCount = 10_000   //encyript it 10000 times
+const charCount = 32   // always the half of desired char - 32 for 64
+const encType = 'sha512'
+
+const passwordEncrypt = function (password) {
+    const encrypted = crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType)
+}
+
 // SCHEMA
 const UserSchema = new mongoose.Schema({
 
@@ -29,6 +42,9 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
+        // set: () => { return 'nininini'}
+        // set: (password) => { return password + 123}
+        // set: function (password) => { return password + 123}
 
     },
 
