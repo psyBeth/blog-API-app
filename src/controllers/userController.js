a1195347d08a653ffc174624f13a583f2c1e31a4
Betul <betulkoru06@gmail.com>
2024-03-11 22:34:37 +0300

    user controller and router

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,46 @@
 'use strict'
+
+require("express-async-errors")
+
+const User = require("../models/userModel")
+
+module.exports = {
+
+    list: async (req, res) => {
+        const data = await User.find()
+        res.status(200).send({
+            error: false,
+            data: data
+        })
+    },
+    create: async (req, res) => {
+        const data = await User.create(req.body)
+        res.status(201).send({
+            error: false,
+            body: req.body,
+            data: data
+        })
+    },
+    read: async (req, res) => {
+        const data = await User.find({ _id: req.params.userId })
+        res.status(202).send({
+            error: false,
+            data: data
+        })
+    },
+    update: async (req, res) => {
+        const data = await User.updateOne({ _id: req.params.userId }, req.body)
+        const newdata = await User.find({ _id: req.params.userId })
+        res.status(202).send({
+            error: false,
+            body: req.body,
+            data: data,
+            newdata: newdata
+        })
+    },
+    delete: async (req, res) => {
+        const data = await User.deleteOne({ _id: req.params.userId })
+        // console.log(data);
+        res.sendStatus((data.deletedCount >= 1) ? 204 : 404)
+    }
+}
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const router = require("express").Router()
+
+const User = require("../controllers/userController")
+
+// User:
+router.route('/users')
+    .get(User.list)
+    .post(User.create)
+router.route('/users/:userId')
+    .get(User.read)
+    .put(User.update) 
+    .patch(User.update)
+    .delete(User.delete)
+
+module.exports = router
